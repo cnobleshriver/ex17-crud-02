d4fb8becef1ce7238abce5739753fc02f97b873f
cnobleshriver <100378265+cnobleshriver@users.noreply.github.com>
2024-04-22 20:15:10 -0400

    first commit

---
+++ src/client/script.js
@@ -0,0 +1,34 @@
+const counterNameInput = document.getElementById("counterName");
+const createBtn = document.getElementById("createBtn");
+const readBtn = document.getElementById("readBtn");
+const updateBtn = document.getElementById("updateBtn");
+const deleteBtn = document.getElementById("deleteBtn");
+const viewAllBtn = document.getElementById("viewAllBtn");
+const counterResponse = document.getElementById("counters");
+
+const URL = "http://localhost:3260"; // URL of our server
+
+// TASK #4: Write event handler functions for each button
+// Function to handle create counter action
+async function createCounter() {}
+
+// TASK #4: Write event handler functions for each button
+// Function to handle read counter action
+async function readCounter() {}
+
+// TASK #4: Write event handler functions for each button
+// Function to handle update counter action
+async function updateCounter() {}
+
+// TASK #4: Write event handler functions for each button
+// Function to handle delete counter action
+async function deleteCounter() {}
+
+// TASK #4: Write event handler functions for each button
+// Function to handle view all counters action
+async function viewAll() {}
+
+// TASK #5: Add event listeners
+
+// Load all counters in DB when page loads
+viewAll();
